fba5077eadf3b226d29024f6012c0dfa559b91ac
Villadshammershoi <villadshammershoei@hotmail.com>
2017-04-10 12:24:42 +0200

    Started project
    Started project

---
+++ The Wall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(The_Wall.Startup))]
+namespace The_Wall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
